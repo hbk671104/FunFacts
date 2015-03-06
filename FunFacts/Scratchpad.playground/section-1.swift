d7e0d25f04ac82311f1221b13e50fe3c5d616b91
hbk671104 <hbk671104@gmail.com>
2015-03-06 18:29:53 -0500

    Done

--- FunFacts/Scratchpad.playground/section-1.swift
+++ FunFacts/Scratchpad.playground/section-1.swift
@@ -25,3 +25,9 @@ optionalString = nil
 // Random Number Generation
 
 var randomNumber = Int(arc4random_uniform(10)) // <- Upper bound
+
+// UIColor
+
+var redColor = UIColor(red: 223/255.0, green: 86/255.0, blue: 94/255.0, alpha: 1.0)
+
+let colorsArray = [redColor]
